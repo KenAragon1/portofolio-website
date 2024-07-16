2ad3a051e67a60388e3e394a07fc7cd3c8b741d9
KenAragon1 <142734032+KenAragon1@users.noreply.github.com>
2024-07-16 20:42:46 +0700

    error fixing

--- app/components/ui/GridBackground.tsx
+++ app/components/ui/GridBackground.tsx
@@ -1,4 +1,10 @@
-const GridBackground = ({ children }) => {
+import { ReactNode } from "react";
+
+type ComponentProps = {
+  children: ReactNode;
+};
+
+const GridBackground: React.FC<ComponentProps> = ({ children }) => {
   return (
     <div className="min-h-screen w-full from bg-dot-gray-500/[0.45] z-[-1] relative flex items-center justify-center">
       {children}
